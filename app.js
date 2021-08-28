d21eb81b093760ad0f87aa5eadb12023f48c488a
ncaruso <nico.caruso1992@outlook.com>
2021-08-28 00:26:16 -0300

    Se agrega UI

--- app.js
+++ app.js
@@ -52,13 +52,14 @@ function whoWonTheGame(playerScore, computerScore) {
     if(round === 5) {
         const finalScore = document.querySelector(".final-score");
         if(playerScore === computerScore) {
-            finalScore.textContent = "It's a tie!!";
+            finalScore.textContent = "It's a tie!";
         }
         if(playerScore < computerScore) {
-            finalScore.textContent = "You lose the game!!";
+            finalScore.textContent = "You lost!";
         } else {
-            finalScore.textContent = "You win the game!!";
+            finalScore.textContent = "You win!";
         }
+        finalScore.classList.toggle("hide");
     }
 }
 
@@ -71,31 +72,40 @@ function resetScore() {
     round = 0;
     scorePlayerOne.textContent = 0;
     scorePlayerTwo.textContent = 0;
-    finalScore.textContent = "";
+    finalScore.classList.toggle("hide");
 }
 
 function choosedBtn(e) {
     return e.target.getAttribute("data-choice") 
 }
 
-function addPoint(result) {
+function addPoint(result, playerInput, computerInput) {
     const scorePlayerOne = document.querySelector(".score-player-one .score");
     const scorePlayerTwo = document.querySelector(".score-player-two .score");
+    const scoreChoiceOne = document.querySelector("#scoreOne");
+    scoreChoiceOne.setAttribute("class", `fas fa-hand-${playerInput}`);
 
-    if(result.indexOf("win") === -1) {
-        computerScore += 1;
-        scorePlayerTwo.textContent = computerScore;
-    } else {
-        playerScore += 1;
-        scorePlayerOne.textContent = playerScore;
-    }
+    const scoreChoiceTwo = document.querySelector("#scoreTwo");
+    scoreChoiceTwo.setAttribute("class", `fas fa-hand-${computerInput}`);
+
+    //if(result.indexOf("tie") === -1) {
+        if(result.indexOf("win") === -1) {
+            computerScore += 1;
+            scorePlayerTwo.textContent = computerScore;
+        } else {
+            playerScore += 1;
+            scorePlayerOne.textContent = playerScore;
+        }
+    //}
+
 }
 
 function playGame(e) {
     //inicia el juego. Partida de 5 turnos.
     const playerInput = choosedBtn(e);
-    const result = playRound(playerInput, computerPlay());
-    addPoint(result);
+    const computerInput = computerPlay();
+    const result = playRound(playerInput, computerInput);
+    addPoint(result, playerInput, computerInput);
 
     return whoWonTheGame(playerScore, computerScore);
 }
